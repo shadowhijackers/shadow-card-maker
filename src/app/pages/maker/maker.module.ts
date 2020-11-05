dcb162cc51759ae13dd91ebf8d48c4674c207816
agiratech-nagaraj <nagaraj@agiratech.com>
2020-11-05 23:16:56 +0530

    Added sample form fields

--- src/app/pages/maker/maker.module.ts
+++ src/app/pages/maker/maker.module.ts
@@ -1,19 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { MakerPageRoutingModule } from './maker-routing.module';
 
 import { MakerPage } from './maker.page';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    MakerPageRoutingModule
+    MakerPageRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatButtonModule
   ],
   declarations: [MakerPage]
 })
