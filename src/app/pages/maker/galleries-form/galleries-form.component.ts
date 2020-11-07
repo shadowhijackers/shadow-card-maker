7072ffbc78a03c5470c5fe4e251e3f69ab774020
agiratech-nagaraj <nagaraj@agiratech.com>
2020-11-07 18:51:29 +0530

    Wip: Updated add image upload functionality in gallery  form

--- src/app/pages/maker/galleries-form/galleries-form.component.ts
+++ src/app/pages/maker/galleries-form/galleries-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-galleries-form',
@@ -6,32 +6,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./galleries-form.component.scss'],
 })
 export class GalleriesFormComponent implements OnInit {
-  uploadedFiles = [];
+  uploadedFiles: Array<UploadedImage> = [];
   uploadedFilesData = [];
+
   private message: string;
-  private imagePath: any;
 
-  constructor() { }
+  constructor() {
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
+
+  trackByFn(value: UploadedImage, index: number){
+    return index;
+  }
 
   loadImageFromDevice(event: Event | any) {
-    const files = event.target.files;
+    const files: FileList = event.target.files;
     if (files.length === 0)
       return;
 
-    const mimeType = files[0].type;
-    if (mimeType.match(/image\/*/) == null) {
-      this.message = "Only images are supported.";
-      return;
-    }
 
-    const reader = new FileReader();
-    this.imagePath = files;
-    reader.readAsDataURL(files[0]);
-    reader.onload = (_event) => {
-      this.uploadedFiles.push(reader.result);
-      this.uploadedFilesData.push(files[0]);
-    }
+    Array.from({length: files.length}, (v, i) => {
+      const file = files.item(i);
+      const mimeType = file.type;
+      if (mimeType.match(/image\/*/) == null) {
+        this.message = "Only images are supported.";
+        return;
+      }
+
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = (_event) => {
+        this.uploadedFiles.push({
+          imgURL: reader.result,
+          caption: '',
+          id:  (1000 * Math.random() + new Date().getTime()).toString(36)
+        });
+        this.uploadedFilesData.push(file);
+      }
+    });
   }
+
+  clearUploadedImg(uploadedImg, idx: number) {
+    this.uploadedFiles.splice(idx, 1);
+  }
+}
+
+export interface UploadedImage {
+  imgURL: any;
+  caption: string;
+  id: string;
 }
