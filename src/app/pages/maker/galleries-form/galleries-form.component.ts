c93682389792ee98c8ae077e8d7f03fb9ca1d4b5
agiratech-nagaraj <nagaraj@agiratech.com>
2020-11-07 17:01:58 +0530

    Wip: Updated add image functionality in gallery  form

--- src/app/pages/maker/galleries-form/galleries-form.component.ts
+++ src/app/pages/maker/galleries-form/galleries-form.component.ts
@@ -6,9 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./galleries-form.component.scss'],
 })
 export class GalleriesFormComponent implements OnInit {
+  uploadedFiles = [];
+  uploadedFilesData = [];
+  private message: string;
+  private imagePath: any;
 
   constructor() { }
 
   ngOnInit() {}
 
+  loadImageFromDevice(event: Event | any) {
+    const files = event.target.files;
+    if (files.length === 0)
+      return;
+
+    const mimeType = files[0].type;
+    if (mimeType.match(/image\/*/) == null) {
+      this.message = "Only images are supported.";
+      return;
+    }
+
+    const reader = new FileReader();
+    this.imagePath = files;
+    reader.readAsDataURL(files[0]);
+    reader.onload = (_event) => {
+      this.uploadedFiles.push(reader.result);
+      this.uploadedFilesData.push(files[0]);
+    }
+  }
 }
